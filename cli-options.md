b735bb545ce2612f8283a164b359816f441fa234
philipnickel <tipkik@hotmail.com>
2025-09-09 10:07:28 +0200

    work from w1

---
+++ cli-options.md
@@ -0,0 +1,1 @@
+module load mpi/5.0.8-gcc-13.4.0-binutils-2.44
--- week_1/Labs_week01/hello_n.c
+++ week_1/Labs_week01/hello_n.c
@@ -8,6 +8,7 @@
  */
 
 #include <stdio.h>
+#include "mpi.h"
 
 int 
 main(int argc, char* argv[]) {
@@ -15,10 +16,11 @@ main(int argc, char* argv[]) {
     int rank, size;
 
     MPI_Init(&argc, &argv);
-    MPI_Comm_rank(...);
-    MPI_Comm_size(...);
 
-    printf("Hello, world...");
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    printf("Hello, world from %d of %d!\n", rank, size);
 
     MPI_Finalize();
 
